cad34412483c4074af3914e57274bf560ad8a519
Mohammadarifcoding <minaraarifa@gmail.com>
2023-12-23 03:46:06 +0600

    Added the functionality

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const Tasks = client.db('TaskFlow').collection('Tasks')
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
 
 
     app.get('/tasks/:email',async(req,res)=>{
@@ -68,8 +68,7 @@ async function run() {
   })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
   } finally {
     // Ensures that the client will close when you finish/error
     
